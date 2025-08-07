7047ba50baf836a6edd7cc00da66429afe7a8b70
syniahpeterson <syniahpeterson@gmail.com>
2025-08-07 19:24:12 -0400

    updated react-redux challenges

--- react-redux/vite.config.js
+++ react-redux/vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   base: '/Frontend-Development-Libraries/react-redux/',
-  plugins: [react()],
-});
+  plugins: [react()]
+})
