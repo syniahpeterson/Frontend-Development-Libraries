f6562b9ebb749589492d07c63d212f900bf91170
syniahpeterson <syniahpeterson@gmail.com>
2025-08-07 20:40:23 -0400

    updated react-redux

--- react-redux/vite.config.js
+++ react-redux/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Frontend-Development-Libraries/react-redux/',
+  base: './',
   plugins: [react()]
-})
+});
