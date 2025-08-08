38f6da32d2f6709a8fd16d9a3f517c9a7af89d5f
syniahpeterson <syniahpeterson@gmail.com>
2025-08-08 19:11:32 -0400

    Add initial CSS styles for the messages app layout and theme

---
+++ react-redux/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Challenge01 from "./challenges/Challenge01.js";
+import Challenge02 from "./challenges/Challenge02.js";
+import Challenge03 from "./challenges/Challenge03.js";
+import Challenge04 from "./challenges/Challenge04.js";
+import Challenge05 from "./challenges/Challenge05.js";
+import Challenge06 from "./challenges/Challenge06.js";
+import Challenge07 from "./challenges/Challenge07.js";
+import Challenge08 from "./challenges/Challenge08.js";
+import Challenge09 from "./challenges/Challenge09.js";
+import MessagesApp from "./challenges/MessagesApp.js";
+
+function App() {
+  return (
+    <div>
+      <h1>React and Redux Challenges</h1>
+      <Challenge01 />
+      <Challenge02 />
+      <Challenge03 />
+      <Challenge04 />
+      <Challenge05 />
+      <Challenge06 />
+      <Challenge07 />
+      <Challenge08 />
+      <Challenge09 />
+      <MessagesApp />
+    </div>
+  );
+}
+
+export default App;
