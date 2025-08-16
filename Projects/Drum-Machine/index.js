493403f70da16df21deeb8f9dd3d49a88cc29b82
syniahpeterson <syniahpeterson@gmail.com>
2025-08-15 20:20:58 -0400

    Added Drum Machine Structure

---
+++ Projects/Drum-Machine/index.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return (
+    <h1>Drum Machine</h1>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
